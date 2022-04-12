189a84276cb4fc46d86392b18a4a61ab49c9a7ee
unknown <macrowell@yahoo.com>
2022-04-12 18:10:19 -0400

    added 5 event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
+
 import './less/index.less'
 
 // Your code goes here!
+const mainNav = document.querySelector(".main-navigation");
+const contHome = document.querySelector(".home");
+
+function navMenuLarger() {
+    mainNav.style.padding = "100px";
+}
+mainNav.addEventListener("mouseover", navMenuLarger);
+
+function navMenuSmaller() {
+    mainNav.style.padding = 0;
+}
+contHome.addEventListener("click", navMenuSmaller)
+
+function esc(evt) {
+    if (evt.key === 'Escape') {
+        alert("There is no escaping the fun bus");
+    }
+}
+
+document.addEventListener("keydown", esc);
+
+const busImg = document.querySelector(".intro img");
+
+function pageChange() {
+    document.querySelector("body").style.color = "red";
+    document.querySelector("body").style.backgroundColor = "black";
+    mainNav.style.color = "red";
+    mainNav.style.backgroundColor = "black";
+}
+
+busImg.addEventListener("dblclick", pageChange);
+
+const h1 = document.querySelector("h1");
+
+function h1Change() {
+    if (mainNav.style.color === "red") {
+    h1.textContent = "The fun bus loves you!";
+ }
+}
+
+document.addEventListener("wheel", h1Change);
+
+
+
+
+
